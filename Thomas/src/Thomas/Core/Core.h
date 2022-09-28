fe31b6f54b50f396ce9a5b7e8d77ce3e93101ccb
Derrick <derrick992110@gmail.com>
2022-09-28 20:57:08 +0800

    UPDATED ONE LINE OF ASSERT

--- Thomas/src/Thomas/Core/Core.h
+++ Thomas/src/Thomas/Core/Core.h
@@ -10,6 +10,19 @@
 	#error Thomas only supports Windows!
 #endif
 
+#ifdef TH_DEBUG
+#define TH_ENABLE_ASSERTS
+#endif
+
+#ifdef TH_ENABLE_ASSERTS
+#define TH_ASSERT(x, ...) { if(!(x)) { TH_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#define TH_CORE_ASSERT(x, ...) { if(!(x)) { TH_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+#define TH_ASSERT(x, ...)
+#define TH_CORE_ASSERT(x, ...)
+#endif
+
+
 #define BIT(x) (1<<x)
 
 #define TH_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
--- Thomas/src/Thomas/Core/application.cpp
+++ Thomas/src/Thomas/Core/application.cpp
@@ -18,7 +18,7 @@ namespace Thomas {
 
 	Application::Application()
 	{
-		//call assert when assert.h is made
+		TH_CORE_ASSERT(!s_Instance, "Application already exists!");
 		s_Instance = this;
 
 		m_Window = std::unique_ptr<Window>(Window::Create());
