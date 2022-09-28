2158c26c6fb3b40ae2e4b1c52bd62efdb4e9bbf8
Derrick <derrick992110@gmail.com>
2022-09-28 20:57:30 +0800

    Update application.cpp

--- Thomas/src/Thomas/Core/application.cpp
+++ Thomas/src/Thomas/Core/application.cpp
@@ -29,57 +29,6 @@ namespace Thomas {
 		
 		Render::init();
 
-		//example triangle 
-		//glGenVertexArrays(1, &m_VertexArray);
-		//glBindVertexArray(m_VertexArray);
-
-		//
-		//float vertices[3 * 3] =
-		//{
-		//	-0.5f,-0.5f,0.0f,
-		//	0.5f,-0.5f,0.0f,
-		//	0.0f,0.5f,0.0f,
-		//};
-
-
-		////m_VertexBuffer.reset(VertexBuffer::Create( vertices , sizeof(vertices) ));
-
-		//glEnableVertexAttribArray(0);
-		//// index 0 no of float 3 normalize false, no of bytes between vertex, 
-		//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE , 3*sizeof(float) , nullptr );
-
-		//unsigned int indices[3] = { 0,1,2 };
-
-		//m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
-
-		/*std::string vertexSrc = R"(
-			#version 330 core
-			
-			layout(location = 0) in vec3 a_Position;
-			out vec3 v_Position;
-			void main()
-			{
-				v_Position = a_Position;
-				gl_Position = vec4(a_Position, 1.0);	
-			}
-		)";
-
-		std::string fragmentSrc = R"(
-			#version 330 core
-			
-			layout(location = 0) out vec4 color;
-			in vec3 v_Position;
-			void main()
-			{
-				color = vec4(v_Position * 0.5 + 0.5, 1.0);
-			}
-		)";*/
-
-		//m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
-		//vertex array
-		//vertex buffer
-		//index buffer
-		//shader
 
 		GameObjectFactory factory;
 		Signature signature;
