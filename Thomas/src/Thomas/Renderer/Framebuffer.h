64c159a3b6151e718913d9692364c175f6ca8d50
Derrick <derrick992110@gmail.com>
2022-10-21 12:30:17 +0800

    framebuffer class

--- Thomas/src/Thomas/Renderer/Framebuffer.h
+++ Thomas/src/Thomas/Renderer/Framebuffer.h
@@ -19,19 +19,31 @@ namespace Thomas
 	class Framebuffer
 	{
 	public:
-		Framebuffer(const FramebufferSpec& spec);
+		std::shared_ptr<Framebuffer> Create(const FramebufferSpec& spec);
 		virtual ~Framebuffer();
 
-		const FramebufferSpec& GetSpec() const;
+		const FramebufferSpec& GetSpec() const { return m_Spec; }
+		uint32_t GetColorAttachmentID() const { return m_ColorAttachment; }
 
 		void Invalidate();
 
+		void Bind();
+		void Unbind();
+
+		Framebuffer(const FramebufferSpec& spec);
+		/*	:m_Spec(spec)
+		{
+			Invalidate();
+		}*/
+
 		//static std::shared_ptr<Framebuffer> Create(const FramebufferSpec& spec);
 		
-		
 	private:
+		
+
 		FramebufferSpec m_Spec;
 		uint32_t m_RendererID;
+		uint32_t m_ColorAttachment, m_DepthAttachment;
 	};
 
 }
