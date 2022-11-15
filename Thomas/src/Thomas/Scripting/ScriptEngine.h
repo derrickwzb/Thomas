cf0ac7950b0e81d0eeb9e465a8af3ee4fe083385
brandonzhuo <90678102+brandonzhuo@users.noreply.github.com>
2022-11-15 23:29:36 +0800

    updated scripts and premake
    LOL

---
+++ Thomas/src/Thomas/Scripting/ScriptEngine.h
@@ -0,0 +1,17 @@
+#pragma once
+namespace Thomas {
+
+    class ScriptEngine {
+
+    public:
+        static void Init();
+        static void Shutdown();
+
+    private:
+        static void InitMono();
+        static void ShutdownMono();
+    };
+
+
+
+}
