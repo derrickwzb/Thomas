c636a90ea775e78f2047798ee426d8fbc10ccee1
brandonzhuo <90678102+brandonzhuo@users.noreply.github.com>
2023-01-16 21:44:11 +0800

    scipting
    scripting

--- Thomas/src/Thomas/Scripting/ScriptGlue.cpp
+++ Thomas/src/Thomas/Scripting/ScriptGlue.cpp
@@ -5,11 +5,16 @@
 #include  "Thomas/Scripting/ScriptEngine.h"
 #include "Thomas/Core/KeyCodes.h"
 #include "Thomas/Core/Input.h"
+#include "Thomas/Scene/Components.h"
 
 #include "mono/metadata/object.h"
+#include "mono/metadata/reflection.h"
 #include "glm/glm.hpp"
 
 namespace Thomas {
+
+    static std::unordered_map<MonoType*, std::function<bool(Entity)>> s_EntityHasComponentFuncs;
+
 
 //Using internal call must have the namespace of where the function is followed by
 //the class or wherever the function is inside the namespace follow by the scope operator
@@ -37,23 +42,31 @@ namespace Thomas {
         return glm::dot(*parameter, *parameter);
     }
     
+    static bool Entity_HasComponent(EntityID entityID, MonoReflectionType* componentType)
+    {
+        Scene* scene = ScriptEngine::GetSceneContext();
+        //TH_CORE_ASSERT(scene);
+
+        Entity entity = { entityID , scene }; //This is the way to find the particular entity for a particular scene
+        //TH_CORE_ASSERT(entity);
+
+        MonoType* managedType = mono_reflection_type_get_type(componentType);
+        //TH_CORE_ASSERT(s_EntityHasComponentFuncs.find(managedType) != s_EntityHasComponentFuncs.end());
+        return s_EntityHasComponentFuncs.at(managedType)(entity);
+    }
+
     static void Entity_GetTranslation(EntityID entityID, glm::vec2* outTranslation)
     { 
-      
         Scene* scene = ScriptEngine::GetSceneContext();
         Entity entity = { entityID , scene }; //This is the way to find the particular entity for a particular scene
-        *outTranslation = entity.GetComponent<Transform>().translation;
-        
+        *outTranslation = entity.GetComponent<Transform>().translation;  
     }
 
     static void Entity_SetTranslation(EntityID entityID, glm::vec3* translation)
     {
-        
         Scene* scene = ScriptEngine::GetSceneContext();
         Entity entity = { entityID , scene }; //This is the way to find the particular entity for a particular scene
         entity.GetComponent<Transform>().translation = *translation;
-       
-
     }
 
     static bool Input_IsKeyDown(int keycode)
@@ -61,12 +74,44 @@ namespace Thomas {
         return Input::IsKeyPressed(keycode);
     }
 
-	void ScriptGlue::RegisterFunctions() 
+    template <typename Component>
+    static void RegisterComponent()
+    {
+        //([]()
+        //{
+
+            std::string_view typeName = typeid(Component).name();
+            size_t pos = typeName.find_last_of(':');
+            std::string_view structName = typeName.substr(pos + 1);
+            std::string managedTypename = fmt::format("Thomas.{}", structName);
+            std::cout << managedTypename;
+
+            MonoType* managedType = mono_reflection_type_from_name(managedTypename.data(), ScriptEngine::GetCoreAssemblyImage());
+            s_EntityHasComponentFuncs[managedType] = [](Entity entity) {return entity.HasComponent<Transform>(); };
+
+        //}(), ...);
+     
+    }
+    /*
+    template <typename ... Component>
+    static void RegisterComponent(ComponentGroup<Component ...>)
+    {
+        RegisterComponent <Component ...> ();
+    }*/
+     
+    void ScriptGlue::RegisterComponents()
+    {
+        //RegisterComponent(AllComponents{});
+        RegisterComponent<Transform>();
+    }
+
+    void ScriptGlue::RegisterFunctions()
 	{
         TH_ADD_INTERNAL_CALL(NativeLog);
         TH_ADD_INTERNAL_CALL(NativeLog_Vector);
         TH_ADD_INTERNAL_CALL(NativeLog_VectorDot);
         
+        TH_ADD_INTERNAL_CALL(Entity_HasComponent);
         TH_ADD_INTERNAL_CALL(Entity_GetTranslation);
         TH_ADD_INTERNAL_CALL(Entity_SetTranslation);
 
--- Thomas/src/Thomas/Scripting/ScriptGlue.h
+++ Thomas/src/Thomas/Scripting/ScriptGlue.h
@@ -5,6 +5,7 @@ namespace Thomas {
 	class ScriptGlue {
 
 	public:
+		static void RegisterComponents();
 		static void RegisterFunctions();
 
 	};
