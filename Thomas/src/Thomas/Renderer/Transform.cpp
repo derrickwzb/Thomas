4c3eaa7a9ef6272ae3736886cd82b9a4910580f7
ahxiong881 <xiong.x@digipen.edu>
2023-02-02 17:55:16 +0800

    Fixed gizmo rotation bug

--- Thomas/src/Thomas/Renderer/Transform.cpp
+++ Thomas/src/Thomas/Renderer/Transform.cpp
@@ -24,7 +24,7 @@ namespace Thomas {
 	// Called to calculate the mdl_to_ndc_xform matrix
 	void Transform::compute_mdl_to_ndc_xform() {
 		glm::mat3 scale_mat = { scaling.x, 0, 0, 0, scaling.y, 0, 0, 0, 1 };
-		glm::mat3 rot_mat = { cos(rotation * (M_PI / 180)), sin(rotation * (M_PI / 180)), 0, -sin(rotation * (M_PI / 180)), cos(rotation * (M_PI / 180)), 0, 0, 0, 1 };
+		glm::mat3 rot_mat = { cos(-rotation), sin(-rotation), 0, -sin(-rotation), cos(-rotation), 0, 0, 0, 1 };
 		glm::mat3 trans_mat = { 1, 0, 0, 0, 1, 0, translation.x, translation.y, 1 };
 		mdl_to_ndc_xform = trans_mat * rot_mat * scale_mat;
 	}
