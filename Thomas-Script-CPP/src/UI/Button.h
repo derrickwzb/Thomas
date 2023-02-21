17964da5a7717fc6598c085daba0326f9c7bb05d
Derrick <derrick992110@gmail.com>
2023-02-21 17:54:03 +0800

    fix

--- Thomas-Script-CPP/src/UI/Button.h
+++ Thomas-Script-CPP/src/UI/Button.h
@@ -5,6 +5,13 @@ class Button : public Thomas::ScriptableEntity
 {
 public:
 
+	bool CheckBounds(float Cursor_X, float Cursor_Y, glm::vec2 min_pos, glm::vec2 max_pos) {
+	if (Cursor_X >= min_pos.x && Cursor_Y >= min_pos.y && Cursor_X <= max_pos.x && Cursor_Y <= max_pos.y)
+		return true;
+	else
+		return false;
+}
+
 	void OnCreate()
 	{
 		TH_CORE_INFO("Button Script Instantiated. ");
@@ -17,7 +24,7 @@ public:
 		auto& data = GetComponent<Thomas::Texture>();
 		auto& trans = GetComponent<Thomas::Transform>();
 
-		/*if (Checkcol(Cursor_X, Cursor_Y, trans.global_min, trans.global_max))
+		if (CheckBounds(Cursor_X, Cursor_Y, trans.global_min, trans.global_max))
 		{
 			if (data.button_hover == false) {
 				data.texid -= 1;
@@ -30,7 +37,7 @@ public:
 				data.texid += 1;
 				data.button_hover = false;
 			}
-		}*/
+		}
 	}
 
 	void OnDestroy()
