d6df5fec6b03a16aefbc55a29121e12b52b21ae1
brandonzhuo <90678102+brandonzhuo@users.noreply.github.com>
2023-03-06 22:01:23 +0800

    audio script
    audio script

---
+++ Thomas-Script-CPP/src/UI/VolumeUI.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "../ScriptUtils.h"
+#include "../Managers/AudioManager.h"
+
+static float vol_bar_min;
+static float vol_bar_max;
+
+class VolumeUI : public Thomas::ScriptableEntity
+{
+	void OnCreate() {
+
+		auto& tag = GetComponent<Thomas::TagComponent>().tag;
+
+		if (tag == "Volume_Fill") {
+
+			auto& trans = GetComponent<Thomas::Transform>();
+
+			float scaling = trans.scaling.x / 2;
+			vol_bar_min = trans.translation.x - scaling;
+			vol_bar_max = trans.translation.x + scaling;
+
+		}
+
+	}
+
+	void OnUpdate(Thomas::Timestep) {
+
+		auto& tag = GetComponent<Thomas::TagComponent>().tag;
+
+		if (tag == "Volume_Control") {
+
+			auto& trans = GetComponent<Thomas::Transform>();
+	
+			if (trans.translation.x <= vol_bar_max - (trans.scaling.x / 2)) {
+				//Check here for issues becuase the translation might be 0
+				trans.translation.x *= (curr_volume / max_volume);
+
+			}
+
+		}
+
+	}
+
+	void OnDestroy() {
+
+
+
+	}
+};
