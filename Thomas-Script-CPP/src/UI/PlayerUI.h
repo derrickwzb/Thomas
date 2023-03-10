b97d18fd95f3d1d5abe17786f6318e1ee79d7989
Cxp-QwQ <90663676+Cxp-QwQ@users.noreply.github.com>
2023-03-10 18:33:34 +0800

    add comments

--- Thomas-Script-CPP/src/UI/PlayerUI.h
+++ Thomas-Script-CPP/src/UI/PlayerUI.h
@@ -1,3 +1,16 @@
+/******************************************************************************/
+/*!
+\file		PlayerUI.h
+\author 	Chen XinPeng
+\email:		c.xinpeng@digipen.edu
+\date   	10/3/2023
+\brief		This file contains the functions and declarations of player ui script.
+
+Copyright (C) 2023 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the prior
+written consent of DigiPen Institute of Technology is prohibited.
+ */
+/******************************************************************************/
 #pragma once
 #include "../ScriptUtils.h"
 #include "../Puddle.h"
